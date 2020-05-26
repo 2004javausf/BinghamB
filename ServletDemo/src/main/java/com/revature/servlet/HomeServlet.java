0879d09fc35d97a188c9baaf2d6d958c2ce835b4
Brad Bingham <bnbingham@hotmail.com>
2020-05-26 00:51:46 -0700

    Added Angular project

--- ServletDemo/src/main/java/com/revature/servlet/HomeServlet.java
+++ ServletDemo/src/main/java/com/revature/servlet/HomeServlet.java
@@ -8,22 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HomeServlet
- */
+
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("login.html").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("In doPost of HomeServlet");
 		response.setContentType("text/html");
